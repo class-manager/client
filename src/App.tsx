09722f60ccee3bae999cba9f60a177e7573d3469
Jared <48422312+lhjt@users.noreply.github.com>
2021-03-22 22:21:36 +1100

    Add carbon-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 /** @jsxImportSource @emotion/react */
-import "./App.css";
-import logo from "./logo.svg";
+import { Header, HeaderName } from "carbon-components-react";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <>
+            <Header>
+                <HeaderName prefix="">Classman</HeaderName>
+            </Header>
+            <main css={{ marginTop: 48 }}>
+                <h1 css={{ padding: "3rem" }}>Welcome to Classman.</h1>
+            </main>
+        </>
     );
 }
 
